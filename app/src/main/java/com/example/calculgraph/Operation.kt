25eb9ef5b71da8968a358069fd15263f02b01af7
andrew <p-a-k-12@mail.ru>
2021-07-25 01:34:29 +0300

    operation change and reverse

--- app/src/main/java/com/example/calculgraph/Operation.kt
+++ app/src/main/java/com/example/calculgraph/Operation.kt
@@ -1,10 +1,23 @@
 package com.example.calculgraph
 
+import com.example.calculgraph.Operation.*
+
 enum class Operation {
     PLUS,
     MINUS,
     MULTIPLICATION,
     DIVISION,
     DEGREE,
-    ROOT
+    ROOT,
+    NONE
+}
+
+fun Operation.reverse() = when(this) {
+    PLUS -> MINUS
+    MINUS -> PLUS
+    MULTIPLICATION -> DIVISION
+    DIVISION -> MULTIPLICATION
+    DEGREE -> ROOT
+    ROOT -> DEGREE
+    NONE -> NONE
 }
