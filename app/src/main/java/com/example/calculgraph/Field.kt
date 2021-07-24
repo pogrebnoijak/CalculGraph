bc1019439923a505815c2163a468ffa831afc301
andrew <p-a-k-12@mail.ru>
2021-07-25 01:35:11 +0300

    field and graph

--- app/src/main/java/com/example/calculgraph/Field.kt
+++ app/src/main/java/com/example/calculgraph/Field.kt
@@ -1,21 +1,45 @@
 package com.example.calculgraph
 
+import java.util.*
+import kotlin.math.pow
 import kotlin.random.Random
+import com.example.calculgraph.Operation.*
 
-class Field(var kolMoves: Int) {
-    private val graph = Graph()
-
-    var time = 60
-
+class Field(var kolMoves: Int, kolNode: Int) {
+    val graph = Graph(kolNode)
     var currentNode = Random.nextInt(0, graph.kolNode)
+    var currentNumber = Random.nextInt(0, 100)
+    val totalNumber = Random.nextInt(0, 100)
+    private val history = Stack<Int>()
 
-    private val history = ArrayDeque<Int>()
-
-    fun move() {
-        TODO()
+    fun move(to: Int) {
+        history.push(currentNode)
+        moving(currentNode, to)
+        currentNode = to
+        kolMoves--
     }
 
     fun back() {
-        TODO()
+        if(history.isNotEmpty()) {
+            kolMoves++
+            val from = currentNode
+            currentNode = history.pop()
+            moving(from, currentNode)
+        }
+        // TODO ("else")
+    }
+
+    private fun moving(from: Int, to: Int) {
+        when(graph.data[from][to].oper) {
+            NONE              -> throw error("wrong move!")
+            PLUS              -> currentNode += graph.data[from][to].num ?: throw error("wrong data num!")
+            MINUS             -> currentNode -= graph.data[from][to].num ?: throw error("wrong data num!")
+            MULTIPLICATION    -> currentNode *= graph.data[from][to].num ?: throw error("wrong data num!")
+            DIVISION          -> if (graph.data[from][to].num == 0) throw error("0 division")
+            else currentNode /= graph.data[from][to].num ?: throw error("wrong data num!")
+            DEGREE            -> graph.data[from][to].num?.let { currentNode.toDouble().pow(it) } ?: throw error("wrong data num!")
+            ROOT              -> if (graph.data[from][to].num == 0) throw error("0 division")
+            else graph.data[from][to].num?.let { currentNode.toDouble().pow(1 / it) } ?: throw error("wrong data num!")
+        }
     }
 }
--- app/src/main/java/com/example/calculgraph/Graph.kt
+++ app/src/main/java/com/example/calculgraph/Graph.kt
@@ -1,13 +1,74 @@
 package com.example.calculgraph
 
-class Graph(val kolNode: Int = 4, val kolBranch: Int = 6) {
-    val data: Array<Array<Pair<Operation, Int>>> = generateGraph()
+import com.example.calculgraph.Operation.*
+import kotlin.random.Random
 
-    private fun generateGraph(): Array<Array<Pair<Operation, Int>>> {
-        TODO()
+class Graph(val kolNode: Int, val kolBranch: Int = 6) {
+    inner class Inscription(val oper: Operation, val num: Int?)
+
+    val data: Array<Array<Inscription>> = generateGraph()
+
+    private fun generateGraph(): Array<Array<Inscription>> {
+        val probList = listOf(PLUS, PLUS, PLUS, MINUS, MINUS, MINUS, MULTIPLICATION, MULTIPLICATION, DIVISION, DIVISION, DEGREE, ROOT)
+        val _data = Array(kolNode) {
+            Array(kolNode) {
+                Inscription(NONE, null)
+            }
+        }
+        val kolInIndex = Array(kolNode) {0}
+
+        fun doBounds(oper: Operation) = when(oper) {
+            PLUS, MINUS     -> Pair(0, 100)
+            MULTIPLICATION  -> Pair(0, 20)
+            DIVISION        -> Pair(1, 20)
+            DEGREE          -> Pair(0, 5)
+            ROOT            -> Pair(1, 5)
+            NONE            -> throw error("unreal")
+        }
+
+        fun generateOne(i: Int) {
+            var j = i
+            while (j == i || _data[i][j].oper != NONE) {
+                j = Random.nextInt(0, kolNode)
+            }
+            val oper = probList[Random.nextInt(0, probList.size)]
+            val num = doBounds(oper).let { Random.nextInt(it.first, it.second) }
+            _data[i][j] = Inscription(oper, num)
+            _data[j][i] = Inscription(oper.reverse(), num)
+            kolInIndex[i]++
+            kolInIndex[j]++
+        }
+
+        for (i in 0 until kolNode - 1) {
+            generateOne(i)
+        }
+        var kol = kolBranch - kolNode + 1
+        if (kolInIndex[kolNode-1] == 0) {
+            generateOne(kolNode - 1)
+            kol--
+        }
+        while (kol != 0) {
+            var i = -1
+            while (i < 0 || kolInIndex[i] == kolNode - 1) {
+                i = Random.nextInt(0, kolNode)
+            }
+            generateOne(i)
+            kol--
+        }
+        return _data
     }
 }
 
-fun correctnessGraph(graph: Graph): Boolean {
-    TODO()
+internal fun correctnessGraph(graph: Graph): Boolean {
+    if (graph.data.size != graph.kolNode || graph.data[0].size != graph.kolNode || graph.kolBranch < graph.kolNode) return false
+    var kol = 0
+    for (i in 0 until graph.kolNode) {
+        for (j in 0 until graph.kolNode) {
+            if (graph.data[i][j].oper == NONE) continue
+            kol++
+            if (graph.data[i][j].oper in listOf(DIVISION, ROOT) && graph.data[i][j].num == 0) return false
+            if (graph.data[i][j].num == null) return false
+        }
+    }
+    return kol == graph.kolBranch
 }
