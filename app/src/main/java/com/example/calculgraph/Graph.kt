b6c742188c52a3985639a02da5e5dfa30f573459
andrew <p-a-k-12@mail.ru>
2021-07-25 18:44:06 +0300

    correctGraph, listTo, fix doBounds

--- app/src/main/java/com/example/calculgraph/Graph.kt
+++ app/src/main/java/com/example/calculgraph/Graph.kt
@@ -4,7 +4,7 @@ import com.example.calculgraph.Operation.*
 import com.example.calculgraph.constant.*
 import kotlin.random.Random
 
-class Graph(val kolNode: Int, val kolBranch: Int = 6) {
+class Graph(val kolNode: Int, kolMoves: Int, currentNode:Int, val kolBranch: Int = 6) {
     inner class Inscription(val oper: Operation, val num: Int?) {
         override fun toString(): String {
             return when(oper) {
@@ -19,27 +19,27 @@ class Graph(val kolNode: Int, val kolBranch: Int = 6) {
         }
     }
 
-    val data: Array<Array<Inscription>> = generateGraph()
+    val data: Array<Array<Inscription>> = generateGraph(kolMoves, currentNode)
 
-    private fun generateGraph(): Array<Array<Inscription>> {
+    fun listTo(data: Array<Array<Inscription>>) = data.map { line ->
+        line.mapIndexed { i, insc -> Pair(i, insc) }
+            .filter { it.second.oper != NONE }
+            .map { it.first }
+    }
+
+    private fun generateGraph(kolMoves: Int, currentNode: Int): Array<Array<Inscription>> {
         fun factorial(n: Int) = (2..n).fold(1L, Long::times)
         if (factorial(kolNode) < kolBranch) throw error("Too match branches!")
 
         val probList = listOf(PLUS, PLUS, PLUS, MINUS, MINUS, MINUS, MULTIPLICATION, MULTIPLICATION, DIVISION, DIVISION, DEGREE, ROOT)
-        val _data = Array(kolNode) {
-            Array(kolNode) {
-                Inscription(NONE, null)
-            }
-        }
+        lateinit var _data: Array<Array<Inscription>>
         val kolInIndex = Array(kolNode) {0}
 
         fun doBounds(oper: Operation) = when(oper) {
-            PLUS, MINUS     -> BOUNDS_PLUS_MINUS
-            MULTIPLICATION  -> BOUNDS_MULTIPLICATION
-            DIVISION        -> BOUNDS_DIVISION
-            DEGREE          -> BOUNDS_DEGREE
-            ROOT            -> BOUNDS_ROOT
-            NONE            -> throw error("unreal")
+            PLUS, MINUS                 -> BOUNDS_PLUS_MINUS
+            MULTIPLICATION, DIVISION    -> BOUNDS_MULTIPLICATION_DIVISION
+            DEGREE, ROOT                -> BOUNDS_DEGREE_ROOT
+            NONE                        -> throw error("unreal")
         }
 
         fun generateOne(i: Int) {
@@ -47,7 +47,7 @@ class Graph(val kolNode: Int, val kolBranch: Int = 6) {
             while (j == i || _data[i][j].oper != NONE) {
                 j = Random.nextInt(0, kolNode)
             }
-            val oper = probList[Random.nextInt(0, probList.size)]
+            val oper = probList.random()
             val num = doBounds(oper).let { Random.nextInt(it.first, it.second) }
             _data[i][j] = Inscription(oper, num)
             _data[j][i] = Inscription(oper.reverse(), num)
@@ -55,26 +55,50 @@ class Graph(val kolNode: Int, val kolBranch: Int = 6) {
             kolInIndex[j]++
         }
 
-        for (i in 0 until kolNode - 1) {
-            generateOne(i)
-        }
-        var kol = kolBranch - kolNode + 1
-        if (kolInIndex[kolNode-1] == 0) {
-            generateOne(kolNode - 1)
-            kol--
-        }
-        while (kol != 0) {
-            var i = -1
-            while (i < 0 || kolInIndex[i] == kolNode - 1) {
-                i = Random.nextInt(0, kolNode)
+        fun correctGraph(): Boolean {
+            val list = listTo(_data)
+
+            fun dfs(cur: Int, last: Int, length: Int): Boolean {
+                if (length == 0) return true
+                for (i in list[cur]) {
+                    if (i == last) continue
+                    if(dfs(i, cur, length - 1)) return true
+                }
+                return false
             }
-            generateOne(i)
-            kol--
+
+            return dfs(currentNode, -1, kolMoves)
         }
+
+
+        do {
+            _data = Array(kolNode) {
+                Array(kolNode) {
+                    Inscription(NONE, null)
+                }
+            }
+            for (i in 0 until kolNode - 1) {
+                generateOne(i)
+            }
+            var kol = kolBranch - kolNode + 1
+            if (kolInIndex[kolNode - 1] == 0) {
+                generateOne(kolNode - 1)
+                kol--
+            }
+            while (kol != 0) {
+                var i = -1
+                while (i < 0 || kolInIndex[i] == kolNode - 1) {
+                    i = Random.nextInt(0, kolNode)
+                }
+                generateOne(i)
+                kol--
+            }
+        } while (!correctGraph())
         return _data
     }
 }
 
+// for tests
 internal fun correctnessGraph(graph: Graph): Boolean {
     if (graph.data.size != graph.kolNode || graph.data[0].size != graph.kolNode || graph.kolBranch < graph.kolNode) return false
     var kol = 0
