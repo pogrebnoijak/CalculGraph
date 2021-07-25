b747beb94772c965ab1ac82750c796482eb83e5d
andrew <p-a-k-12@mail.ru>
2021-07-25 12:42:22 +0300

    add constants

---
+++ app/src/main/java/com/example/calculgraph/constant/Constants.kt
@@ -0,0 +1,31 @@
+package com.example.calculgraph.constant
+
+// const
+const val DRAWING = 10L         // ms
+const val LAYOUT_CONST = 0.7F
+
+// point
+const val SMALL_WIDTH = 3F
+const val AVERAGE_WIDTH = 5F
+const val LARGE_WIDTH = 7F
+
+// changing
+const val RIBS_POSITION = 0.2F
+const val RAD_K = 0.9F
+const val RAD_IN_K = 0.7F
+const val RAD_MINI_K = 0.1F
+const val RAD_INNER_K = 0.3F
+const val TEXT_SIZE_K = 0.08F
+const val TEXT_SIZE_BIG_K = 0.1F
+
+// params
+const val ALL_TIME = 60_000L     // ms
+const val KOL_MOVES = 2
+const val KOL_NODES = 5
+const val CURRENT_NUMBER_MAX = 100
+const val CURRENT_TOTAL_MAX = 100
+val BOUNDS_PLUS_MINUS = Pair(0, 100)
+val BOUNDS_MULTIPLICATION = Pair(0, 20)
+val BOUNDS_DIVISION = Pair(1, 20)
+val BOUNDS_DEGREE = Pair(0, 5)
+val BOUNDS_ROOT = Pair(1, 5)
