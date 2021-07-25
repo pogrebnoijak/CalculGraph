43f1e17494dc916239e1a944a4cd779ed295b8f4
andrew <p-a-k-12@mail.ru>
2021-07-25 12:44:02 +0300

    branches numbering

--- app/src/main/java/com/example/calculgraph/GameActivity.kt
+++ app/src/main/java/com/example/calculgraph/GameActivity.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.calculgraph.constant.*
 import java.util.*
 import kotlin.math.PI
 import kotlin.math.cos
@@ -17,11 +18,9 @@ import kotlin.math.sin
 
 
 class GameActivity : AnyActivity() {
-    private val drawing = 10L       // ms
     private var play = true
     private lateinit var background: DrawView
     private lateinit var curField: Field
-    private var allTime = 60_000L   // ms
     private var time = 0L           // ms
 
     inner class Size(val width: Float, val height: Float)
@@ -29,7 +28,7 @@ class GameActivity : AnyActivity() {
         get() {
             val metrics = DisplayMetrics()
             windowManager.defaultDisplay.getMetrics(metrics)
-            return Size(metrics.widthPixels * 1F, metrics.heightPixels * 0.7F)
+            return Size(metrics.widthPixels * 1F, metrics.heightPixels * LAYOUT_CONST)
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,9 +68,9 @@ class GameActivity : AnyActivity() {
 
     @SuppressLint("SetTextI18n")
     private fun doField() {
-        curField = Field(2,4)
+        curField = Field(KOL_MOVES,KOL_NODES)
         findViewById<TextView>(R.id.kolMoves).text = "moves: ${curField.kolMoves}"
-        findViewById<TextView>(R.id.totalNumber).text = "need to get ${curField.totalNumber}!"
+        findViewById<TextView>(R.id.totalNumber).text = "need to get ${curField.totalNumber}"
     }
 
     private fun setRendering() {
@@ -79,13 +78,13 @@ class GameActivity : AnyActivity() {
         val motion = object : TimerTask() {
             override fun run() {
                 if(play) {
-                    time += drawing
+                    time += DRAWING
                     background.invalidate()
-                    if (time >= allTime) play = false
+                    if (time >= ALL_TIME) play = false
                 }
             }
         }
-        timer.schedule(motion, 0, drawing)
+        timer.schedule(motion, 0, DRAWING)
     }
 
 
@@ -93,10 +92,10 @@ class GameActivity : AnyActivity() {
         val p: Paint = Paint()
         val centerH = size.height / 2
         val centerW = size.width / 2
-        val rad = centerW * 0.9F
-        val radIn = centerW * 0.7F
-        val radMini = centerW * 0.1F
-        val radInner = centerW * 0.3F
+        val rad = centerW * RAD_K
+        val radIn = centerW * RAD_IN_K
+        val radMini = centerW * RAD_MINI_K
+        val radInner = centerW * RAD_INNER_K
         val rect = RectF(centerW - rad, centerH - rad, centerW + rad, centerH + rad)
         val centers: List<Pair<Float, Float>>
             get() = (0 until curField.graph.kolNode).map { 2.0 * it / curField.graph.kolNode }
@@ -108,13 +107,24 @@ class GameActivity : AnyActivity() {
         override fun onDraw(canvas: Canvas) {
             canvas.run {
                 p.apply {
-                    color = Color.MAGENTA
-                    strokeWidth = 5F
+                    strokeWidth = AVERAGE_WIDTH
+                    textSize = centerW * TEXT_SIZE_K
+                    textAlign = Paint.Align.CENTER
                 }
                 for (i in 0 until curField.graph.kolNode) {
                     for (j in 0 until curField.graph.kolNode) {
                         if (curField.graph.data[i][j].oper == Operation.NONE) continue
-                        if (i > j) drawLine(centers[i].first, centers[i].second, centers[j].first, centers[j].second, p)
+                        if (i > j) {
+                            p.color = Color.MAGENTA
+                            drawLine(centers[i].first, centers[i].second, centers[j].first, centers[j].second, p)
+                            val vec = Pair(centers[j].first - centers[i].first, centers[j].second - centers[i].second) // it's so normal to calculate because it's constant
+                            p.color = Color.GREEN
+//                            TODO("write normal root")
+                            drawText(curField.graph.data[i][j].toString(), centers[i].first + RIBS_POSITION * vec.first,
+                                centers[i].second + RIBS_POSITION * vec.second - (p.descent() + p.ascent())/2, p)
+                            drawText(curField.graph.data[j][i].toString(), centers[i].first + (1 - RIBS_POSITION) * vec.first,
+                                centers[i].second + (1 - RIBS_POSITION) * vec.second - (p.descent() + p.ascent())/2, p)
+                        }
                     }
                 }
 
@@ -134,22 +144,23 @@ class GameActivity : AnyActivity() {
     }
 
     private inner class DrawView(context: Context?) : Draws(context) {
+        @SuppressLint("ResourceAsColor")
         override fun onDraw(canvas: Canvas) {
             canvas.run {
                 p.apply {
-                    strokeWidth = 3F
+                    strokeWidth = SMALL_WIDTH
                     color = Color.GREEN
-                    textSize = centerW * 0.1F
+                    textSize = centerW * TEXT_SIZE_BIG_K
                     textAlign = Paint.Align.CENTER
                 }
                 drawText(curField.currentNumber.toString(), centerW, centerH - (p.descent() + p.ascent())/2, p)
 
                 p.apply {
-                    strokeWidth = 7F
-                    color = Color.rgb(50, 50, 140)
+                    strokeWidth = LARGE_WIDTH
+                    color = R.color.color3
                     style = Paint.Style.STROKE
                 }
-                drawArc(rect, -90F, time * 360F / allTime, false, p)
+                drawArc(rect, -90F, time * 360F / ALL_TIME, false, p)
 
                 p.apply {
                     color = Color.YELLOW
